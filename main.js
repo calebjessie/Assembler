f19bc7341c17e7e8476aefcd8ae9ceba02156e16
Club <cjjessie793@gmail.com>
2017-01-14 12:32:36 -0500

    Added some html elements. Created feed file.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
-let win
+let win;
 
 function createWindow () {
-	win = new BrowserWindow({width: 800, height: 600, frame: false})
+	win = new BrowserWindow({width: 1024, height: 768, frame: false});
 	
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
@@ -16,20 +16,20 @@ function createWindow () {
 	win.webContents.openDevTools()
 	
 	win.on('closed', () => {
-		win = null
+		win = null;
 	})
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
-		app.quit()
+		app.quit();
 	}
 })
 
 app.on('activate', () => {
 	if (win === null) {
-		createWindow()
+		createWindow();
 	}
 })
