a1956bbf0d641b047b788ba34abb80448d3eaecc
Club <cjjessie793@gmail.com>
2017-03-01 18:23:55 -0500

    Updated a lot. Added images in feed and lazy load. New design in psd.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+'use strict'
+const {ipcRenderer} = require('electron');
 const remote = require('electron').remote;
-const fs = require('fs'),
-	  path = require('path');
+
 
 // Create window controls
 (function() {
@@ -22,57 +23,72 @@ const fs = require('fs'),
 })();
 
 
-
 // Create directory browse
 const browseDir = function() {
+	
+	document.getElementById('asset-feed').innerHTML = '<div id="browse">Start by browsing for your assets folder. <button id="browse-files">Browse</button></div>';
+	
 	document.getElementById('browse-files').addEventListener('click', () => {
-		var filePaths = remote.dialog.showOpenDialog({
-			properties: ['openDirectory']
-		});
-		for (var filePath of filePaths) {
-			console.log(filePath);
+		// ipc message
+		ipcRenderer.send('loadAssets');
+	});
+	
+	ipcRenderer.on('getAssets', (event, arg) => {
+		for (let i = 0; i < 20; i++) {
+			displayAssets(arg[i].file);
 		}
+		console.log(document.getElementById('asset-feed').clientHeight);
 		
-		walk(filePath, function(curPath, stat) {
-			console.log(curPath);
+		document.getElementById('browse').style.display = 'none';
+		document.getElementById('container').addEventListener('scroll', () => {
+			let container = document.getElementById('container');
+			
+			if ((container.clientHeight + container.scrollTop) >= container.scrollHeight) {
+				renderFileImages(20, 20, arg);
+				console.log('Works');
+			}
 		});
 	});
 }
+
+
+// Create html for each asset
+function displayAssets(path) {
+	let divImg = document.createElement('div');
+	let bgImg = 'url("' + path.replace(/\\/g,"/") + '")';
+	
+	divImg.className = 'asset-img';
+	divImg.style.backgroundImage = bgImg;
+	
+	document.getElementById('asset-feed').appendChild(divImg);
+}
+
+
+// Display more assets on scroll
+function renderFileImages(startPosition, length, array) {
+	for(let i = startPosition; i < startPosition + length; i++) {
+		displayAssets(array[i].file);
+	}
+}
+
+
+// Save array to JSON
+/*function saveJSON(array) {
+	let addFiles = JSON.stringify(array, null, "\t");
+	
+	fs.writeFile('files.json', addFiles, (err) => {
+		if (err) throw err;
+		console.log("Saved file.");
+	});
+}*/
+
+
+// Check if user has selected a directory
+/*if (allFiles.length != 0) {
+	console.log("Files have been loaded");
+} else {
+	browseDir();
+}*/
+
 
 browseDir();
-
-function walk(currentDirPath, callback) {
-	fs.readdir(currentDirPath, (err, files) => {
-		files.forEach(file => {
-			var curPath = path.join(currentDirPath, file);
-			var stat = fs.statSync(curPath);
-			if (stat.isFile()) {
-				callback(curPath, stat);
-			} else if (stat.isDirectory()) {
-				walk(curPath, callback);
-			}
-		})
-	});
-}
-
-
-
-// Has the user chosen a asset directory?
-//if () {
-	// Display feed
-//} else {
-	// User browses for directory
-	// Create directory browse
-/*	const browseDir = function() {
-		document.getElementById('browse-files').addEventListener('click', () => {
-			let filePaths = remote.dialog.showOpenDialog({
-				properties: ['openDirectory']
-			});
-			for (let filePath of filePaths) {
-				console.log(filePath);
-			}
-		});
-	}
-
-	browseDir();*/
-//}
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain, dialog} = require('electron');
 const path = require('path'),
-	  url = require('url');
+	  url = require('url'),
+	  fs = require('fs');
 
 let win;
+let allFiles = [];
+
 
 function createWindow () {
-	win = new BrowserWindow({width: 1024, height: 768, frame: false});
+	win = new BrowserWindow({width: 1124, height: 768, frame: false});
 	
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
@@ -20,6 +23,11 @@ function createWindow () {
 	})
 }
 
+// Send window information
+ipcMain.on('window', (event, args) => {
+	event.sender.send('getWin', win);
+})
+
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
@@ -33,3 +41,43 @@ app.on('activate', () => {
 		createWindow();
 	}
 })
+
+
+// Load Assets
+ipcMain.on('loadAssets', (event, args) => {
+	var filePaths = dialog.showOpenDialog({
+		properties: ['openDirectory']
+	});
+	for (var filePath of filePaths) {
+		console.log(filePath);
+	}
+
+	walkSync(filePath, (curPath, stat, fileType) => {
+		allFiles.push({
+			file: curPath,
+			type: fileType
+		});
+	});
+
+	//saveJSON(allFiles);
+	
+	event.sender.send('getAssets', allFiles);
+})
+
+
+// Walk through all directories to find paths
+function walkSync(currentDirPath, callback) {
+	fs.readdirSync(currentDirPath).forEach(file => {
+		let curPath = path.join(currentDirPath, file);
+		let stat = fs.statSync(curPath);
+		let fileType = path.extname(curPath);
+		if (stat.isFile()) {
+			if (fileType == '.jpg') {
+				callback(curPath, stat, fileType);
+			}
+			//callback(curPath, stat, fileType);
+		} else if (stat.isDirectory()) {
+			walkSync(curPath, callback);
+		}
+	});
+}
